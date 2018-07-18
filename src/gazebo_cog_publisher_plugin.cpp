dc212b848a9d37a47bc107e82db969931d66693a
Masaya Kataoka <ms.kataoka@gmail.com>
2018-07-18 20:56:15 +0900

    remove header

--- src/gazebo_cog_publisher_plugin.cpp
+++ src/gazebo_cog_publisher_plugin.cpp
@@ -1,1 +1,26 @@
-#include <gazebo_cog_publisher_plugin.h>
+//headers in gazebo
+#include <gazebo/gazebo.hh>
+#include <gazebo/physics/physics.hh>
+#include <gazebo/common/common.hh>
+#include <ignition/math/Vector3.hh>
+
+namespace gazebo
+{
+    class gazebo_cog_publisher_plugin : public ModelPlugin
+    {
+        public:
+            gazebo_cog_publisher_plugin()
+            {
+
+            }
+            ~gazebo_cog_publisher_plugin()
+            {
+
+            }
+            public: void Load(physics::ModelPtr /*_parent*/, sdf::ElementPtr /*_sdf*/)
+            {
+
+            }
+    };
+    GZ_REGISTER_MODEL_PLUGIN(gazebo_cog_publisher_plugin)
+}
